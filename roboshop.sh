33d2070cfdddc689e86f6204240b652e8aa987dc
Sriteja <nsriteja4u@gmail.com>
2026-02-06 11:13:11 -0500

    automating roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -6,7 +6,7 @@ AMI_ID="ami-0220d79f3f480ecf5"
 
 for instance in $@
 do
-INSTANCE_ID=$(aws ec2 run-instances \
+    INSTANCE_ID=$(aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type t3.micro \
     --security-group-ids $SG_ID \
@@ -14,7 +14,7 @@ INSTANCE_ID=$(aws ec2 run-instances \
     --query 'Instances[0].InstanceId'\
     --output text)
 
-    if [$instance == "frontend"] then
+    if [$instance == "frontend"]; then
        IP = $(
             aws ec2 describe-instances \
             --instance-ids $INSTANCE_ID \
