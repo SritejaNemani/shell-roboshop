22854e11faca79fabaa154697e2b8d1b266d8a0f
Sriteja <nsriteja4u@gmail.com>
2026-02-09 01:13:28 -0500

    roboshop

--- cart.sh
+++ cart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+: '
 
 USER_ID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
@@ -8,7 +9,7 @@ G="\e[32m" #green
 Y="\e[33m" #yellow
 N="\e[0m" #normal-white
 SCRIPT_DIR=$PWD
-#MONGODB_HOST=mongodb.nemani.online
+MONGODB_HOST=mongodb.nemani.online
 
 if [ $USER_ID -ne 0 ]; then
    echo -e "$R Not root user - please run this script with root user$N" | tee -a $LOGS_FILE
@@ -52,9 +53,9 @@ echo "Creating App Directory"
 mkdir -p /app &>>$LOGS_FILE
 VALIDATE $? "Creating App Directory"
 
-echo "Downloading catalog code"
+echo "Downloading cart code"
 curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOGS_FILE
-VALIDATE $? "Downloading catalog code"
+VALIDATE $? "Downloading cart code"
 
 echo "Opening app Directory"
 cd /app 
@@ -90,3 +91,75 @@ echo "Starting Cart Server"
 systemctl start cart &>>$LOGS_FILE
 VALIDATE $? "Starting Cart Server"
 
+'
+
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.nemani.online
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Disabling NodeJS Default version"
+
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $? "Enabling NodeJS 20"
+
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Install NodeJS"
+
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+    VALIDATE $? "Creating system user"
+else
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip  &>>$LOGS_FILE
+VALIDATE $? "Downloading cart code"
+
+cd /app
+VALIDATE $? "Moving to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+
+unzip /tmp/cart.zip &>>$LOGS_FILE
+VALIDATE $? "Uzip cart code"
+
+npm install  &>>$LOGS_FILE
+VALIDATE $? "Installing dependencies"
+
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
+VALIDATE $? "Created systemctl service"
+
+systemctl daemon-reload
+systemctl enable cart  &>>$LOGS_FILE
+systemctl start cart
+VALIDATE $? "Starting and enabling cart"
