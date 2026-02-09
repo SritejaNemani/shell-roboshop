6189451d22c2f4612986f197dc7e349eb53a0736
Sriteja <nsriteja4u@gmail.com>
2026-02-09 02:29:41 -0500

    roboshop

--- shipping.sh
+++ shipping.sh
@@ -91,11 +91,26 @@ echo "Installing MySQL client"
 dnf install mysql -y &>>$LOGS_FILE
 VALIDATE $? "Installing MySQL client"
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
+#we have toi check before loading the data -  check if a schema exists or not 
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOGS_FILE
+mysql -h mysql.nemani.online -uroot -pRoboShop@1 -e 'use cities' #logins and checks if cities exists or not
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
+if [ $? -eq 0]; then
+    echo -e " data is already loaded $Y Skipping $N"
+else
+    echo "Loading the data"
+
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
+
+
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOGS_FILE
+
+
+    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
+
+    VALIDATE $? "Loading the data"
+
+fi
 
 echo "Restarting Shipping Server"
 systemctl restart shipping
